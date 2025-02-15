1d5e0c33eaa1fc35dd6bbac808b46766de62703c
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:44:24 -0600

    Ejercicio22

--- Ejercicio22/Program.cs
+++ Ejercicio22/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const double PI = 3.141592653589793;
+        double radio = 5.0;
+        double area = PI * Math.Pow(radio, 2);
+        double perimetro = 2 * PI * radio;
+
+        Console.WriteLine("Área: " + area);
+        Console.WriteLine("Perímetro: " + perimetro);
+    }
+}
